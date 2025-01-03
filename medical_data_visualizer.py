ba1a7295af89c251fb547b09d508123d66d62093
ashmeet <ashmeetchhabra111@gmail.com>
2025-01-03 07:19:13 +0000

    figuring out last error

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -21,36 +21,29 @@ df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
 df.loc[df['gluc'] == 1, 'gluc'] = 0
 df.loc[df['gluc'] > 1, 'gluc'] = 1
 
-df_cat = pd.melt(df, id_vars='cardio', value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 
-'overweight'])
-df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='count')
-# print(df_cat)
-sns.catplot(x='variable', y='count', hue='value', col='cardio', data=df_cat, kind='bar')
-plt.show()
-
 
 # 4
 def draw_cat_plot():
     # 5
     # df_cat = None
-    df_cat = pd.melt(df, value_vars=['cholestrol', 'gluc', 'smoke', 'alco', 'active', 
+    df_cat = pd.melt(df, value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 
 'overweight'])
 
 
     # 6
     # df_cat = None
-    df_cat = pd.melt(df, id_vars='cardio', value_vars=['cholestrol', 'gluc', 'smoke', 'alco', 'active', 
+    df_cat = pd.melt(df, id_vars='cardio', value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 
 'overweight'])
     df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size()
     
 
     # 7
-    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='count')
-    sns.catplot(x='variable', y='count', hue='value', col='cardio', data=df_cat, kind='bar')
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar')
     plt.show()
 
     # 8
-    fig = None
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar').fig
 
 
     # 9
@@ -61,21 +54,30 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    # df_heat = None
+    df_heat = df[df['ap_lo'] <= df['ap_hi']]
+    df_heat = df_heat[df_heat['height'] <= df_heat['height'].quantile(0.9725)]
+    df_heat = df_heat[df_heat['weight'] >= df_heat['weight'].quantile(0.025)]
+    df_heat = df_heat[df_heat['weight'] <= df_heat['weight'].quantile(0.9725)]
+    df_heat = df_heat[df_heat['height'] >= df_heat['height'].quantile(0.025)]
+
 
     # 12
-    corr = None
+    # corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    # mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    # fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 6))
 
     # 15
-
+    sns.heatmap(corr, mask=mask)
 
 
     # 16
